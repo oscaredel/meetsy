7601259c818c562c68c6d628907b9d2f8a15be0a
Oscar Edel <edeloscar@gmail.com>
2020-09-30 22:52:01 +0200

    Adjust style for event page and add e-mail validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ApplicationRecord
-  has_many :responses
+  has_many :responses, dependent: :destroy
   validates :name, :description, :organiser_name, :organiser_email, presence: true
 end
