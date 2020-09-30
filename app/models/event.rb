9c648404033237ab18e6705ff71d5dc42814f1fd
Oscar Edel <edeloscar@gmail.com>
2020-09-30 22:54:50 +0200

    add email validation to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
   has_many :responses, dependent: :destroy
   validates :name, :description, :organiser_name, :organiser_email, presence: true
+
+  validates :organiser_email,
+            format: { with: /(.+)@(.+)/, message: "not valid" },
+            uniqueness: { case_sensitive: false, scope: :event }
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -2,9 +2,7 @@ class Response < ApplicationRecord
   belongs_to :event
 
   enum attendance: { yes: '1', maybe: '2', no: '0' }
-  validates :attendance, presence: true
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :attendance, :name, :email, presence: true
 
   validates :email,
             format: { with: /(.+)@(.+)/, message: "not valid" },
