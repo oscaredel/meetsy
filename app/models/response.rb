22918a1576de6bffaa983a6c0c59511a4b505032
Oscar Edel <edeloscar@gmail.com>
2020-10-01 16:38:37 +0200

    fix response enum and form so it saves attendance when editing

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,7 +1,7 @@
 class Response < ApplicationRecord
   belongs_to :event
 
-  enum attendance: { yes: '1', maybe: '2', no: '0' }
+  enum attendance: { yes: 1, maybe: 2, no: 0 }
   validates :attendance, :name, :email, presence: true
 
   validates :email,
