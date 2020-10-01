bb0cbf8561fc09a1b4f2896058bbb6c9d6ed2462
Oscar Edel <edeloscar@gmail.com>
2020-10-01 12:46:01 +0200

    remove uniqueness validation for event.organiser_email

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,5 @@ class Event < ApplicationRecord
   validates :name, :description, :organiser_name, :organiser_email, presence: true
 
   validates :organiser_email,
-            format: { with: /(.+)@(.+)/, message: "not valid" },
-            uniqueness: { case_sensitive: false }
+            format: { with: /(.+)@(.+)/, message: "not valid" }
 end
