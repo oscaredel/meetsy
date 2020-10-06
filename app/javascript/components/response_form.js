e010a302b7830a4a6ec3f312266c464590a5e04e
Oscar Edel <edeloscar@gmail.com>
2020-10-06 22:25:58 +0200

    expand response form if any attendence button is checked, also with validation errors

--- app/javascript/components/response_form.js
+++ app/javascript/components/response_form.js
@@ -1,9 +1,16 @@
 const responseForm = () => {
-  const response_buttons = document.querySelectorAll(".collection_radio_buttons");
+  // expand response form if attendence button is checked
+  const response_buttons = document.querySelectorAll(".radio_buttons");
   response_buttons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-    document.querySelector(".response-form-inputs").classList.remove('d-none');
+    // if button is clicked
+    button.addEventListener("change", (event) => {
+      document.querySelector(".response-form-inputs").classList.remove('d-none');
     });
+
+    // if button is already checked but validations failed
+    if (button.checked == true){
+      document.querySelector(".response-form-inputs").classList.remove('d-none');
+    };
   });
 };
 
