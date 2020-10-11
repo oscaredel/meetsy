c00331d6f06ac445f85880426011b1a1f6a865dd
Oscar Edel <edeloscar@gmail.com>
2020-10-11 17:51:33 +0200

    Add create update action

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -1,2 +1,20 @@
 class UpdatesController < ApplicationController
+
+  def create
+    @update = Update.new(update_params)
+    @update.event = Event.find(params[:event_id])
+    @update.contact = Contact.find(session[:uuid])
+
+    if @update.save
+      redirect_to event_path(@update.event)
+    else
+      render 'events/show'
+    end
+  end
+
+  private
+
+  def update_params
+    params.require(:update).permit(:text)
+  end
 end
